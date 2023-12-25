f65dc08663cd262c8aefaecb08c82a4ed9c62d01
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-25 16:48:33 +0530

    Solved some 2D array problems

---
+++ 2D-Arrays/WavePrint.cpp
@@ -0,0 +1,25 @@
+//question link
+//https://www.codingninjas.com/studio/problems/print-like-a-wave_893268?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_6
+
+using namespace std;
+
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+    for (int col = 0; col < mCols; col++){
+        //if it is odd index column
+        if (col&1){
+            for (int row = nRows - 1; row >= 0; row--){
+                ans.push_back(arr[row][col]);
+            }
+        }
+        //if it is even index column
+        else{
+            for (int row = 0; row < nRows; row++){
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}

690698bd2ccc7616ee4d9ae7980c4504d9a0b7c6
sagar samvedi <sagarsamvedi31@gmail.com>
2024-04-28 16:35:54 +0530

    added files to learn-the-basics

--- learn-the-basics/armstrong.py
+++ learn-the-basics/armstrong.py
@@ -1,14 +1,13 @@
-bool checkArmstrong(int n){
-	int count = int(log10(n) + 1);
-	int temp = n;
-	int sum = 0;
-	while (temp>0){
-		int digit = temp%10;
-		sum += pow(digit,count);
-		temp /= 10;
-	}
-	if (sum == n){
-		return true;
-	}
-	return false;
-}
+import math
+
+def check_armstrong(n):
+    count = int(math.log10(n) + 1)
+    temp = n
+    sum = 0
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** count
+        temp //= 10
+    if sum == n:
+        return True
+    return False
